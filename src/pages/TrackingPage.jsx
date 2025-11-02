62390e1fb8b7be82a78c73449103a7d29282edf8
Iniyaa21 <iniyaapaari@gmail.com>
2025-11-02 13:23:24 +0530

    Render order details in TrackingPage

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -1,9 +1,31 @@
+import axios from "axios";
+import dayjs from "dayjs";
 import { Header } from "../components/Header";
 import { useParams } from "react-router";
+import { useState, useEffect } from "react";
 import "./TrackingPage.css";
 
 export function TrackingPage({ cart }) {
   const { orderId, productId } = useParams();
+  const [order, setOrder] = useState(null);
+
+  useEffect(() => {
+    const fetchTrackingData = async () => {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`
+      );
+      setOrder(response.data);
+    };
+    fetchTrackingData();
+  }, [orderId]);
+
+  if (!order) {
+    return null;
+  }
+
+  const orderProduct = order.products.find((orderProduct) => {
+    return orderProduct.productId === productId;
+  });
   return (
     <>
       <link
@@ -23,17 +45,20 @@ export function TrackingPage({ cart }) {
             View all orders
           </a>
 
-          <div className="delivery-date">Arriving on Monday, June 13</div>
-
-          <div className="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+          <div className="delivery-date">
+            Arriving on{" "}
+            {dayjs(orderProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
 
-          <div className="product-info">Quantity: 1</div>
+          <div className="product-info">
+            {orderProduct.product.name}
+          </div>
+
+          <div className="product-info">Quantity: {orderProduct.quantity}</div>
 
           <img
             className="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg"
+            src={orderProduct.product.image}
           />
 
           <div className="progress-labels-container">
