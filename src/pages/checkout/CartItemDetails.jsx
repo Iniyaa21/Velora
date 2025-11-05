b961f1e9052acc7bec4f76e0f5d5823be25f0d00
Iniyaa21 <iniyaapaari@gmail.com>
2025-11-05 20:00:34 +0530

    Add Update Quantity feature to checkout page

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,11 +1,24 @@
 import axios from "axios";
 import { formatMoney } from "../../utils/money";
+import { useState } from "react";
 
 export function CartItemDetails({ cartItem, loadCart }) {
+  const [isQuantityBeingChanged, setIsQuantityBeingChanged] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+  const changeQuantity = async () => {
+    setIsQuantityBeingChanged(!isQuantityBeingChanged);
+    if (isQuantityBeingChanged) {
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity,
+      });
+      await loadCart();
+      setIsQuantityBeingChanged(false);
+    }
+  };
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
     await loadCart();
-  }
+  };
   return (
     <>
       <img
@@ -20,9 +33,26 @@ export function CartItemDetails({ cartItem, loadCart }) {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            {isQuantityBeingChanged ? (
+              <input
+                type="text"
+                className="update-quantity-input"
+                value={quantity}
+                onChange={(event) => {
+                  let quantityInput = Number(event.target.value);
+                  setQuantity(quantityInput);
+                }}
+              />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={changeQuantity}
+          >
+            Update
+          </span>
           <span
             className="delete-quantity-link link-primary"
             onClick={deleteCartItem}
