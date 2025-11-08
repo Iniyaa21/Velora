c8434818d46fbc10fcc6818d4c142aaa909e1f8e
Iniyaa21 <iniyaapaari@gmail.com>
2025-11-08 20:37:04 +0530

    Modify formatMoney to work with negative numbers and add tests

--- src/utils/money.js
+++ src/utils/money.js
@@ -1,3 +1,7 @@
 export function formatMoney(amountCents) {
+  if (amountCents < 0) {
+    let positiveAmount = amountCents * -1;
+    return `-$${(positiveAmount / 100).toFixed(2)}`;
+  }
   return `$${(amountCents / 100).toFixed(2)}`;
 }
